ed423f27ad9a9bd935e414cb9df75ee962e2fe84
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-04-24 22:16:43 +0300

    Добавил примитивный обработчик ошибок

---
+++ src/main/java/uk/gorodny/alexandernevskyapi/api/advice/ControllerExceptionHandler.java
@@ -0,0 +1,14 @@
+package uk.gorodny.alexandernevskyapi.api.advice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception e) {
+        return ResponseEntity.badRequest().body(new Error(e.getMessage()));
+    }
+}
---
+++ src/main/java/uk/gorodny/alexandernevskyapi/api/advice/Error.java
@@ -0,0 +1,13 @@
+package uk.gorodny.alexandernevskyapi.api.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Error {
+
+    private String error;
+}
