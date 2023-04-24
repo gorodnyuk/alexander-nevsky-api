995098530b3685957066babb5458e2215be826ce
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-04-24 10:43:38 +0300

    Init Commit

---
+++ src/main/java/uk/gorodny/alexandernevskyapi/AlexanderNevskyApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gorodny.alexandernevskyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexanderNevskyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexanderNevskyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gorodny/alexandernevskyapi/AlexanderNevskyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gorodny.alexandernevskyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexanderNevskyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
